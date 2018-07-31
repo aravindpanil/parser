72f514b6eba9f797c85c7a0557d548d847eff4bb
aravindpanil <aravindpanil@gmail.com>
2018-07-31 22:01:26 +0530

    Fix newline bug with scanning character

--- basic.c
+++ basic.c
@@ -6,6 +6,16 @@ int main(){
     float b;
     printf("Enter basic integer and float input\n");
     scanf("%d%f",&a,&b);
-    printf("%d\n%f",a,b);
+    printf("%d\n%f\n",a,b);
+    printf("Enter character\n");
+
+    /*scanf for %c does not strip away whitespace characters where %d does.
+    Hence, when you type previous input it scans. Then you press enter which
+    goes into the input. Hence scanf reads the enter or newline first thus
+    giving you a blank. To avoid this, use " %c" instead of "%c". This tells 
+    scanf with character to avoid any trailing whitespace and newline.
+    */
+    scanf(" %c",&c);
+    printf("%c",c);
     return(0);
 }
