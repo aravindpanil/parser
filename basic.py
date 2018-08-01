443211dfb3dd6580347bee6f03fcd381ea36a6bf
aravindpanil <aravindpanil@gmail.com>
2018-08-01 21:08:32 +0530

    Add python character input

--- basic.py
+++ basic.py
@@ -3,8 +3,12 @@ def parse():
 
 if __name__ ==  "__main__": 
     ans = int(input("Enter an integer\n"))
-    print(ans)
+    print("The integer is",ans)
 
     ansf = float(input("Enter a float number\n"))
     print(ansf)
+    
+    char = input("Enter a character\n")
+    c  = char[0]
+    print(c)
     
