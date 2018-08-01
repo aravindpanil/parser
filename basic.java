d11d90922b0928adbfd3ac1842387e7f38d11c68
aravindpanil <aravindpanil@gmail.com>
2018-08-01 22:04:20 +0530

    Add java equivalent for basic

---
+++ basic.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class basic {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter an integer");
+        int a = sc.nextInt();
+        System.out.println("The integer is "+a);
+
+        System.out.println("Enter a float number");
+        float g = sc.nextFloat();
+        System.out.println(g);
+        
+        System.out.println("Enter a character");
+        String s = sc.next();
+        char c = s.charAt(0);
+        System.out.println(c);
+    }
+}
