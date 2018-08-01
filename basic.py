f6a71a39b1e77c51b0f5484d57be228d8be056e3
aravindpanil <aravindpanil@gmail.com>
2018-08-01 20:42:09 +0530

    Add basic for python

---
+++ basic.py
@@ -0,0 +1,10 @@
+def parse():
+    print("Hello")
+
+if __name__ ==  "__main__": 
+    ans = int(input("Enter an integer\n"))
+    print(ans)
+
+    ansf = float(input("Enter a float number\n"))
+    print(ansf)
+    
