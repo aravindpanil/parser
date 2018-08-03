0a362ef7a10e24123ad272707507cdfdf8bc3e44
aravindpanil <aravindpanil@gmail.com>
2018-08-03 23:29:17 +0530

    Optimized to take any input

--- alternate_convert.c
+++ alternate_convert.c
@@ -1,20 +1,31 @@
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 #define SIZE 100
 int main() {
-    char str[SIZE], *p = str, *ep = NULL, c;
+    char str[SIZE], *p = str, *ep = NULL;
     printf("Enter numbers with any delimiter\n");
     fgets(str, 100, stdin);
-    int li, i = 0;
+    int li, i;
 
+    for (i = strlen(str) - 1; i > 0; i--) {
+        if (isdigit(str[i])) {
+            break;
+        }
+
+        else
+            str[i] = '\0';
+    }
+
+    printf("%s\n", str);
     do {
         li = strtol(p, &ep, 10);
         printf("%d\n", li);
         p = ep;
-        while (!(*p >= '0' && *p <= '9')) p++;
-    } while (*ep != '\n');
+        while (!(isdigit(*p))) p++;
+    } while (*ep != '\0');
 
     return 0;
 }
